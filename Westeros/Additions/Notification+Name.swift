bdfb758802fe8b3c99cad82aecf850ac388e5615
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-10-10 03:03:01 -0300

    Add extension Notification+Name with static  property houseDidChangeNotification

---
+++ Westeros/Additions/Notification+Name.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+Name.swift
+//  Westeros
+//
+//  Created by Luis Herrera Lillo on 10-10-18.
+//  Copyright © 2018 Luis Herrera Lillo. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let houseDidChangeNotification = Notification.Name(Constants.HouseDidChangeNotificationName)
+}
